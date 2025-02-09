85b9d2d607b349fb065d96de5acf4e2ead85b8b2
kgrillaert <92243382+kgrillaert@users.noreply.github.com>
2025-02-09 21:39:14 +0000

    Deploying to gh-pages from @ kgrillaert/kgrillaert.github.io@31877794d74a6bb80cf3cb2236e2ada31277811d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "projects",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-repositories",
+  },{id: "nav-repositories",
           title: "repositories",
           description: "",
           section: "Navigation",
